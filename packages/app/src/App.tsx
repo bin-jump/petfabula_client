8e542d6086e49003d7360da8cc158bec65b21974
zhang <zhang.xs.titech@gmail.com>
2021-11-12 22:16:47 +0900

    fix android toast issue

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -3,18 +3,20 @@ import AppScreen from "./navigations";
 import { I18nextProvider } from "react-i18next";
 import { AppThemeProvider, i18n } from "./providers";
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
-
+import { RootSiblingParent } from "react-native-root-siblings";
 import { StateProvider } from "@petfabula/common";
 
 export default function App() {
   return (
     <BottomSheetModalProvider>
       <I18nextProvider i18n={i18n}>
-        <StateProvider>
-          <AppThemeProvider>
-            <AppScreen />
-          </AppThemeProvider>
-        </StateProvider>
+        <RootSiblingParent>
+          <StateProvider>
+            <AppThemeProvider>
+              <AppScreen />
+            </AppThemeProvider>
+          </StateProvider>
+        </RootSiblingParent>
       </I18nextProvider>
     </BottomSheetModalProvider>
   );
